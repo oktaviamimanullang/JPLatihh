41dd6b41bf61741228460b33cc2fbcd644f4e3d9
oktaviami manullang <61672688+oktaviamimanullang@users.noreply.github.com>
2020-03-23 21:03:06 +0700

    Add files via upload

---
+++ Swiing1/src/com/OktaviamiManullang/penjumlahan/Swiing1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.OktaviamiManullang.penjumlahan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Swiing1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmUtama utama=new FrmUtama();
+        utama.setVisible(true);
+    }
+    
+}
